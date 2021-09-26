a861f785a6197b31143c9609e7bba7bce54ca9d1
chenjinhuo <chenjinhuo@bytedance.com>
2021-09-26 18:39:17 +0800

    feat(wx-mini-uni-app):add debug true

--- wx-mini-uni-app/src/main.js
+++ wx-mini-uni-app/src/main.js
@@ -5,16 +5,16 @@ import App from './App';
 
 const MitoInstance = init(
   {
+    debug: true,
     dsn: 'https://test.com/yourInterface',
-    //
     maxBreadcrumbs: 100,
     // 配置上报时的请求头
     configReportWxRequest(data) {
       return {
         header: {
           'Content-Type': 'text/plain;charset=UTF-8',
-          dataType: 'text',
         },
+        dataType: 'text',
       };
     },
     vue: Vue,
